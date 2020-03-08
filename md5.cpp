409fe5d3322edcfb9dfe631262a1d4d84d1a8db7
sjain24 <jain170107062>
2020-03-08 10:23:48 +0530

    code layout

--- md5.cpp
+++ md5.cpp
@@ -2,6 +2,28 @@
 using namespace std;
 int main()
 {
-	//code
+	// take input
+	string msg;
+	getline(cin, msg);
+	
+	//convert to bits
+
+
+	//pad bits
+
+
+	//add length bits
+
+
+
+	//call F-block procedure
+
+	//call G-block procedure
+
+	//call H-block procedure
+
+	//call I-block procedure
+
+	//store msg digest
 	return 0;
 }
