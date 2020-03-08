63b1c1535cf314b21ad99d9ccc52f88f5f2ad76c
AnamitraMandal <anamitramandal2008@gmail.com>
2020-03-08 13:24:46 +0530

    Revert "Padding added"
    This reverts commit 579a11841d9df9885e9ec97f936ac30be4c433bb.

--- md5.cpp
+++ md5.cpp
@@ -1,19 +1,66 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//function to convert binary sequence to string
+string BinarytoString(string bit_stream){
+	stringstream sstream(bit_stream);
+	string output;
+	while (sstream.good())
+	{
+		bitset<8> bits;
+		sstream >> bits;
+		char c = char(bits.to_ulong());
+		output += c;
+	}
+
+	cout << output;
+	return output;
+}
+
 int main()
 {
-	// take input
+	//1. take input///////////
+	//we assume that msg string is not greater than 512 bits in size
 	string msg;
 	getline(cin, msg);
 	
+	//2. padding//////////////
 	//convert to bits
+	cout << "binary sequence of msg string is: ";
+	for (size_t i = 0; i < msg.size(); ++i)
+	{
+		//print out the bit sequence of msg string
+		cout << bitset<8>(msg.c_str()[i]);
+	}
+	cout<<endl;
 
+	int len = msg.size();//calculate length of message string
+	int len_bits = len*8;//length in bits (1 char = 1 byte = 8 bits)
 
-	//pad bits
+	//calculate no of bits to be padded at the end
+	int len_pad = 512 - 64 - len_bits;
+	
+	string pad_string_bit_seq = "1";
+	for(int i = 0 ; i < len_pad; i++){
+		pad_string_bit_seq += "0";
+	}
 
+	//convert string binary seqeunce to string
+	string pad_string = BinarytoString(pad_string_bit_seq);
+	string msg_padded = msg + pad_string;//append the padded character sequence at the end of msg
 
-	//add length bits
+	cout<<"padded msg string is :";
+	cout<<msg_padded<<endl;
+	cout << "binary sequence of padded msg string is: ";
+	//check if the string is correct by printing the bit sequence
+	// for (size_t i = 0; i < msg_padded.size(); ++i)
+	// {
+	// 	//print out the bit sequence of msg string
+	// 	cout << bitset<8>(msg_padded.c_str()[i]);
+	// }
+	// cout << endl;
+
+	//3. add length bits////////////
 
 
 
