6e6ac251357f44912191b0ed1c542a5da8991003
sjain24 <jain170107062@iitg.ac.in>
2020-04-26 16:36:15 +0530

    rol paramters

--- ARMV8.s
+++ ARMV8.s
@@ -57,27 +57,14 @@ ProcessInnerLoop    :   LDR X11 [X23, X9]//X11 = M[p]
 EndofFunct:             LSL X13, X9, #3//X13 = 16*p
                         ADD X13, X13, X10//X13 = 16*p+q
                         LDR X13, [X21 , X13]//X13 = K[16*p+q]
-
-//X18 will store result of func(abcd) 
-//Assume X0 = 0b0000_0000_0000_0000_1111_1111_1111_1111
-IfpEqual0           :   AND X18,X14,X15
-                        EOR X14,X14,X0//not operation 
-                        AND X14,X14,X16
-                        ORR X18,X18,
-                        B EndofFunct
-IfpEqual1           :   AND X18,X14,X16
-                        EOR X17,X17,X0//not operation 
-                        AND X17,X17,X15
-                        ORR X18,X18,X17
-                        B EndofFunct
-IfpEqual2           :   AND X18,X15,X16
-                        EOR X18,X18,X17//xor operation 
-                        B EndofFunct
-IfpEqual3           :   EOR X17,X17,X0//not operation 
-                        ORR X18,X15,X17
-                        EOR X18,X18,X16
-                        B EndofFunct
-
+                        //compute word[g]
+                        LDR X17, [X26 , X11] //X1 = word[g]
+                        LDR X14, [X20, #0] //X14 = abcd[0]
+                        ADD X17,X17,X13 
+                        ADD X17,X17,X14
+                        ADD X17,X17,X14
+                        ADD X17,X17,X18 // first parameter for rol function
+                        //X1 is 2nd parameter to rol function
 EndOfFComputation   :   ADDI X10, X10, #1
                         SUBIS XZR, X10, #16
                         B.EQ EndOfInnerLoop
@@ -92,6 +79,45 @@ EndOfOuterLoop      :
 
 EndOfFunctionMD5Hash    
 
+//X18 will store result of func(abcd) 
+//Assume X0 = 0b0000_0000_0000_0000_1111_1111_1111_1111
+//X1 will get result of rot
+IfpEqual0           :   AND X18,X14,X15
+                        EOR X14,X14,X0//not operation 
+                        AND X14,X14,X16
+                        ORR X18,X18,X14
 
+                        AND X14,X10,#3 //X14 = q%4
+                        ADDI X14,X14,#0
+                        LDR X1,[X25,x14] //rot[0][q%4]
+                        
+                        B EndofFunct
+
+IfpEqual1           :   AND X18,X14,X16
+                        EOR X17,X17,X0//not operation 
+                        AND X17,X17,X15
+                        ORR X18,X18,X17
+
+                        AND X14,X10,#3 //X14 = q%4
+                        ADDI X14,X14,#4
+                        LDR X1,[X25,x14] //rot[1][q%4]
+                        B EndofFunct
+
+IfpEqual2           :   AND X18,X15,X16
+                        EOR X18,X18,X17//xor operation 
+
+                        AND X14,X10,#3 //X14 = q%4
+                        ADDI X14,X14,#8
+                        LDR X1,[X25,x14] //rot[2][q%4]
+                        B EndofFunct
+
+IfpEqual3           :   EOR X17,X17,X0//not operation 
+                        ORR X18,X15,X17
+                        EOR X18,X18,X16
+
+                        AND X14,X10,#3 //X14 = q%4
+                        ADDI X14,X14,#12
+                        LDR X1,[X25,x14] //rot[3][q%4]
+                        B EndofFunct
 
 
